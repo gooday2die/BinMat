92f45be025c57f70cd73745ad91678aec5143d40
gooday2die <edina00@naver.com>
2022-05-01 15:24:24 +0900

    Edited comments a bit

--- BinMat.cpp
+++ BinMat.cpp
@@ -7,6 +7,11 @@
 
 #include "BinMat.h"
 
+/**
+ * A constructor member function for class BinMAt
+ * @param argX the uint32_t that represents width of binary matrix
+ * @param argY the uint32_t that represents height of binary matrix
+ */
 BinMat::BinMat(uint32_t argX, uint32_t argY){
 	uint32_t totalElements = argX * argY;
 	uint32_t totalDataCount = (totalElements / 32) + 1;
@@ -18,6 +23,12 @@ BinMat::BinMat(uint32_t argX, uint32_t argY){
 	for (uint32_t i = 0 ; i < totalDataCount ; i++) this->data[i] = 0x00000000;
 }
 
+/**
+ * A member function that retrieves specific value from matrix
+ * @param argX the uint32_t that represents x of binary matrix
+ * @param argY the uint32_t that represents y of binary matrix
+ * @return returns uint8_t value that represents binary value from matrix
+ */
 uint8_t BinMat::get(uint32_t argX, uint32_t argY){	
 	if ((argX >= this->x) || (argY >= this->y)) throw BinMat::OutOfRangeException(); 
 	uint32_t dataIndex = (this->x * argY + argX) / 32;
@@ -26,10 +37,18 @@ uint8_t BinMat::get(uint32_t argX, uint32_t argY){
 	return ((tmpData >> dataSegmentIndex) & 0x1);
 }
 
+/**
+ * A member function that sets specific value from matrix
+ * @param argX the uint32_t that represents x of binary matrix
+ * @param argY the uint32_t that represents y of binary matrix
+ * @param value the uint8_t that represents value of binary matrix
+ * @return returns uint8_t value that represents binary value from matrix
+ */
 void BinMat::set(uint32_t argX, uint32_t argY, uint8_t value){
 	if ((argX >= this->x) || (argY >= this->y)) throw BinMat::OutOfRangeException(); 
 	uint32_t dataIndex = (this->x * argY + argX) / 32;
 	uint32_t dataSegmentIndex = ((this->x * argY + argX) % 32);
+	value = value != 0;
 	uint32_t tmpData = (value << dataSegmentIndex);
 	uint32_t tmpData2 = ~(1 << dataSegmentIndex);
 	this->data[dataIndex] = ((this->data[dataIndex] & tmpData2) | tmpData);
--- main.cpp
+++ main.cpp
@@ -1,17 +1,11 @@
 #include <iostream>
 #include "BinMat.h"
+#include <stdio.h>
 
 int main(void) {
-	BinMat a = BinMat(10, 10);
-	for (uint8_t i = 0 ; i < 10 ; i++){
-		for (uint32_t j = 0 ; j < 10 ; j++) a.set(j, i, 1);
-	}
-	a.set(0, 0, 0);
-	a.set(4, 6, 0);
-	for (uint8_t i = 0 ; i < 10 ; i++){
-		for (uint32_t j = 0; j < 10 ; j++) 
-			printf("%d ", a.get(j, i));
-		printf("\n");
+	BinMat a = BinMat(10000, 10000);
+	for (uint32_t i = 0 ; i < 10000 ; i++){
+		for (uint32_t j = 0 ; j < 10000 ; j++) a.set(j, i, 1);
 	}
 	return 0;
 }
